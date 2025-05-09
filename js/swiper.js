a9e87b8c0497cb49dc255761498bce8026a60f96
puritechlab1155 <pub@puritechlab.com>
2025-05-09 19:28:23 +0900

    index수정

--- js/swiper.js
+++ js/swiper.js
@@ -1,39 +1,62 @@
-/* main_visual */
+/* main_visual Swiper */
 var mvSwiper = new Swiper(".mv-swiper", {
+    loop: true, // 무한 루프
+    autoplay: {
+        delay: 3000, // 3초 간격 자동재생
+        disableOnInteraction: false, // 유저 조작 후에도 자동 재생 유지
+    },
     navigation: {
-        nextEl: ".mv-swiper-button-next",
+        nextEl: ".mv-swiper-button-next", // 고유 버튼 클래스
         prevEl: ".mv-swiper-button-prev",
     },
     pagination: {
-        el: ".mv-swiper-pagination",
+        el: ".mv-swiper-pagination", // 고유 페이지네이션 클래스
+        clickable: true, // bullet 클릭 가능
     },
+    effect: "fade", // 페이드 전환 효과
 });
 
-
-// mainIndex section03 + section04
-const swiper = new Swiper('.swiper-container', {
+/* section03 Swiper */
+const swiperSection03 = new Swiper('.swiper-container.section03', {
     slidesPerView: 1,
     spaceBetween: 0,
-    loop: true, // 무한 루프 활성화
+    loop: true,
     pagination: {
-        el: '.swiper-pagination',
+        el: '.sec03-swiper-pagination',
         clickable: true,
     },
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.sec03-swiper-button-next',
+        prevEl: '.sec03-swiper-button-prev',
     },
 });
 
-/* section04 */
-const swiperSection04 = new Swiper('.mainIndex .section04 .swiper-container', {
+/* section04 Swiper */
+const swiperSection04 = new Swiper('.swiper-container.section04', {
     slidesPerView: 'auto',
     centeredSlides: true,
     spaceBetween: 10,
     loop: true,
     navigation: {
-        nextEl: '.mainIndex .section04 .swiper-button-next',
-        prevEl: '.mainIndex .section04 .swiper-button-prev',
+      nextEl: '.sec04-swiper-button-next',
+      prevEl: '.sec04-swiper-button-prev',
     },
-    slideToClickedSlide: true, // 클릭 시 해당 슬라이드로 이동
-});
+    slideToClickedSlide: true,
+    on: {
+      init: function () {
+        updateSection04Text(this);
+      },
+      slideChange: function () {
+        updateSection04Text(this);
+      }
+    }
+  });
+  
+  function updateSection04Text(swiper) {
+    const activeSlide = swiper.slides[swiper.activeIndex];
+    const title = activeSlide.getAttribute('data-title') || '';
+    const date = activeSlide.getAttribute('data-date') || '';
+  
+    document.querySelector('.swiper-text-container .swiper-title').textContent = title;
+    document.querySelector('.swiper-text-container .swiper-date').textContent = date;
+  }
